e2715512d304928918bb4a06b1bbfc17839c43af
Akshen <54845610+Akshen22@users.noreply.github.com>
2020-07-16 21:31:14 +0530

    Add files via upload

--- Example1.java
+++ Example1.java
@@ -1,23 +1,20 @@
-package boxing;
+package exceptionHandling;
 
-//In Java, values from -128 to 127 are cached, 
-//so the same objects are returned. 
-//The implementation of valueOf() uses cached 
-//objects if the value is between -128 to 127.
+import java.util.Scanner;
 
 public class Example1 {
-	
-	public static String compareWc(Integer x,Integer y){
-		return (x==y)?"Same":"Not same";
-	}
 
 	public static void main(String[] args) {
-		Integer x = 40, y = 40;
-		Integer a=400, b=400;
-		Integer m=new Integer(40);
-		Integer n=new Integer(40);
-		System.out.println("40 , 40 Interger are: "+compareWc(x, y));
-		System.out.println("400 , 400 Interger are: "+compareWc(a, b));
-		System.out.println("40 , 40 Interger on explicit object creation : "+compareWc(m, n));
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int a;
+		try {
+			a=sc.nextInt();
+		}catch(Exception e) {
+			System.out.println("Invalid Number!");
+			a=0;
+		}
+		System.out.println("Value of a: "+a);
+		sc.close();
 	}
 }
